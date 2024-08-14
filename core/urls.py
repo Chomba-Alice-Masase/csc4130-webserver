10e4d13f2d1a5aa6bbd7595b54b65d2550c69f52
_pyArisu <alicemasase@gmail.com>
2024-08-14 12:21:57 +0200

    more additions

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
-from .views import SensorDataCreateAPIView, SensorDataListAPIView, TempView
+from .views import SensorDataCreateAPIView, SensorDataListAPIView, TempView, LatestTemperatureData
 
 urlpatterns = [
     path('sensor-data/', SensorDataListAPIView.as_view(), name='sensor-data-list'),
     path('sensor-data/create/', SensorDataCreateAPIView.as_view(), name='sensor-data-create'),
     path('', TempView.as_view(), name='home'),
+    path('latest-temperature-data/', LatestTemperatureData.as_view(), name='latest-temperature-data'),
+
 ]
 
--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import SensorData
 from .serializers import SensorDataSerializer
 from django.views import View
+from django.http import JsonResponse
 
 
 class SensorDataCreateAPIView(generics.CreateAPIView):
@@ -23,3 +24,9 @@ class TempView(View):
 
         context = {'sensor_data': sensor_data}
         return render(request, self.template_name, context)
+
+class LatestTemperatureData(View):
+    def get(self, request):
+        sensor_data = SensorData.objects.all().order_by('-id')[:10]  # Fetch last 10 readings
+        data = list(sensor_data.values('temperature', 'id'))
+        return JsonResponse(data, safe=False)
